478345484b895edbd4ae4b3d39970b74383bea44
Zzcnick <zzhen@stuy.edu>
2016-01-06 19:50:46 -0500

    Add transpose, change equals, extend main method

--- Matrix.java
+++ Matrix.java
@@ -6,53 +6,55 @@
 /*====================================
   class Matrix -- models a square matrix
 
-  TASK: Implement methods below.
-        Categorize runtime of each. 
-        Test in your main method.
+  Runtime Classifications:
+  Constructors: O(1)
+  size():       O(1)
+  get():        O(1)
+  getRow():     O(1)
+  getCol():     O(n)
+  set():        O(1)
+  setRow():     O(1)
+  setCol():     O(n)
+  isFull():     O(n^2)
+  isEmpty():    O(1)
+  toString():   O(n^2)
+  equals():     O(n^2)
+  swapRows():   O(1)
+  swapColumns():O(n)
+  transpose():  O(n)
+  contains():   O(n^2)
   ====================================*/ 
 
 public class Matrix {
-
+    // INSTANCE VARIABLES ----------------------------------------
     //constant for default matrix size
     private final static int DEFAULT_SIZE = 2;
     private Object[][] matrix;
-
 
     //default constructor intializes a DEFAULT_SIZE*DEFAULT_SIZE matrix
     public Matrix( ) {
 	matrix = new Object[DEFAULT_SIZE][DEFAULT_SIZE];
     }
-
-
     //constructor intializes an a*a matrix
     public Matrix( int a ) {
 	this();
 	matrix = new Object[a][a];
     }
 
-
     //return size of this matrix, where size is 1 dimension
     private int size() {
 	return matrix.length;
     }
 
+    // ACCESSORS & MUTATORS ----------------------------------------
     //return the item at the specified row & column   
     private Object get( int r, int c ) {
 	return matrix[r][c];
     }
-
     //return row r
     public Object[] getRow( int r ) {
 	return matrix[r];
     }
-
-    //set row r to newRow, return old row
-    public Object[] setRow( int r, Object[] newRow ) {
-	Object[] ret = getRow(r);
-	matrix[r] = newRow;
-	return ret;
-    }
-
     //return column c
     public Object[] getCol( int c ) {
 	Object[] ret = new Object[size()];
@@ -62,6 +64,19 @@ public class Matrix {
 	return ret;
     }
 
+    //overwrite item at specified row and column with newVal
+    //return old value
+    private Object set( int r, int c, Object newVal ) {
+	Object old = get(r,c);
+	matrix[r][c] = newVal;
+	return old;
+    }
+    //set row r to newRow, return old row
+    public Object[] setRow( int r, Object[] newRow ) {
+	Object[] ret = getRow(r);
+	matrix[r] = newRow;
+	return ret;
+    }
     //set column c to newCol, return old column
     public Object[] setCol( int c, Object[] newCol ) {
 	Object[] ret = getCol(c);
@@ -71,7 +86,8 @@ public class Matrix {
 	}
 	return ret;
     }
-
+    
+    // METHODS ----------------------------------------
     //return true if all cells are filled, false otherwise
     private boolean isFull() {
 	for (int i = 0; i < matrix.length; i++) {
@@ -82,20 +98,11 @@ public class Matrix {
 	}
 	return true;
     }
+    
     // return true if a specified cell of the matrix is empty, false otherwise
     private boolean isEmpty( int r, int c ) {
 	return get(r,c) == null;
     }
-
-
-    //overwrite item at specified row and column with newVal
-    //return old value
-    private Object set( int r, int c, Object newVal ) {
-	Object old = get(r,c);
-	matrix[r][c] = newVal;
-	return old;
-    }
-
 
     //return String representation of this matrix
     // (make it look like a matrix)
@@ -111,30 +118,27 @@ public class Matrix {
 	return ret;
     }
 
-
     //override inherited equals method
     //criteria for equality: matrices have identical dimensions,
     // and identical values in each slot
     public boolean equals( Object rightSide ) {	
-	boolean ret = rightSide == this;
-	if (!(ret)) {
-	    if (rightSide instanceof Matrix) {
-		int size = this.size();
-		if (size == ((Matrix)rightSide).size()) {
-		    for (int r = 0; r < size; r++) {
-			for (int c = 0; c < size; c++) {
-			    if (!(this.get(r,c).equals(((Matrix)rightSide).get(r,c))))
-				return false;
-			}
+	if (rightSide == this) 
+	    return true;
+	if (rightSide instanceof Matrix) {
+	    Matrix r = (Matrix)rightSide;
+	    if (r.size() == this.size()) {
+		for (int i = 0; i < this.size(); i++) {
+		    for (int j = 0; j < this.size(); j++) {
+			if (!(r.get(i,j) == null && this.get(i,j) == null) &&
+			    !(r.get(i,j).equals(this.get(i,j))))
+			    return false;
 		    }
-		    return true;
 		}
+		return true;
 	    }
-	    ret = false;
-	}
-	return ret;
+	} 
+	return false;
     }
-
 
     //swap two columns of this matrix 
     //(0,0) is top left corner of matrix
@@ -149,7 +153,6 @@ public class Matrix {
 	}
     }
 
-
     //swap two rows of this matrix 
     //(0,0) is top left corner of matrix
     //row values increase going down
@@ -158,6 +161,16 @@ public class Matrix {
 	Object[] old_r1 = matrix[r1];
 	matrix[r1] = matrix[r2];
 	matrix[r2] = old_r1;
+    }
+
+    //transposes the matrix (flips matrix across center diagonal)
+    public void transpose() {
+	int size = this.size();
+	Object[][] ret = new Object[size][size];
+	for (int i = 0; i < size; i++) {
+	    ret[i] = this.getCol(i);
+	}
+	matrix = ret;
     }
 
     //returns true if matrix contains o, false otherwise
@@ -211,6 +224,25 @@ public class Matrix {
 	System.out.println("Matrix 3: \n" + m3);
 	System.out.println("Matrix 3 equals Matrix 1:\n" + m3.equals(m1));
 	System.out.println("Matrix 3 equals Matrix 2:\n" + m3.equals(m2));
+
+	// SetCol, SetRow:
+	m1.setRow(0, new Object[]{1,1});
+	m1.setRow(1, new Object[]{2,2});
+	m3.setCol(0, new Object[]{1,1});
+	m3.setCol(1, new Object[]{2,2});
+	System.out.println("Matrix 1 After Setting: \n" + m1);
+	System.out.println("Matrix 3 After Setting: \n" + m3);
+
+	// Transpose:
+	m1.transpose();
+	System.out.println("Matrix 1 After Transposing: \n" + m1);
+	System.out.println("Matrix 3: \n" + m3);
+	System.out.println("Matrix 3 equals Matrix 1:\n" + m3.equals(m1));
+
+	// Contains:
+	System.out.println("Matrix 1 Contains 1: \n" + m1.contains(1));
+	System.out.println("Matrix 1 Contains 2: \n" + m1.contains(2));
+	System.out.println("Matrix 1 Contains 3: \n" + m1.contains(3));
     }
 
 }//end class Matrix
